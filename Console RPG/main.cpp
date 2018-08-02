9b1be01484191293e60a9b8e50e1c2e1569bf78c
KrutPatel2257 <krut@kruts-macbook-pro.local>
2018-08-02 15:01:14 +0530

    Initial Commit

---
+++ Console RPG/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Console RPG
+//
+//  Created by Krut Patel on 02/08/18.
+//  Copyright © 2018 Krut Patel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
