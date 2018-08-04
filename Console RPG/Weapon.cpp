022d35630051913729250bc8dc41b79562d515c8
KrutPatel2257 <krutpatel22577@gmail.com>
2018-08-04 17:22:52 +0530

    Weapon Class

---
+++ Console RPG/Weapon.cpp
@@ -0,0 +1,17 @@
+//
+//  Weapon.cpp
+//  Console RPG
+//
+//  Created by Krut Patel on 04/08/18.
+//  Copyright © 2018 Krut Patel. All rights reserved.
+//
+
+#include "Weapon.hpp"
+
+Weapon::Weapon(){
+    
+}
+
+Weapon::~Weapon(){
+    
+}
---
+++ Console RPG/Weapon.hpp
@@ -0,0 +1,18 @@
+//
+//  Weapon.hpp
+//  Console RPG
+//
+//  Created by Krut Patel on 04/08/18.
+//  Copyright © 2018 Krut Patel. All rights reserved.
+//
+
+#ifndef Weapon_hpp
+#define Weapon_hpp
+
+class Weapon {
+public:
+    Weapon();
+    virtual ~Weapon();
+};
+
+#endif /* Weapon_hpp */
